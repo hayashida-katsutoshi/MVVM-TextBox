02000e65616e1b5aa797f623fa0653054656cbab
Harry <hk.13701@konamigaming.com>
2021-11-05 17:21:37 -0700

    init

---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using MvvmGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace MVVM_TextBox.ViewModels
+{
+    [ViewModel]
+    public partial class MainPageViewModel
+    {
+        [Property] string _localPath = "";
+
+        [Command]
+        private async void SelectLocalPath()
+        {
+            var picker = new FolderPicker();
+            picker.FileTypeFilter.Add("*");
+            var folder = await picker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                LocalPath = folder.Path;
+            }
+        }
+    }
+}
